0446f4c597d9c0af34ceddfa918fca41c9e90db0
Paul CODREA <paulcodrea@users.noreply.github.com>
2023-02-21 20:46:52 +0000

    draft

---
+++ basic_hello_world/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	// fmt is a package that implements formatted I/O with functions analogous
+	// to C's printf and scanf.
+	"fmt"
+)
+
+func main() {
+
+	var str = "Hello World"
+	fmt.Println(str)
+}
